0455423cf771c1d8d0be5fe6e581a46ea9068a2f
DynaTiuM <56697513+DynaTiuM@users.noreply.github.com>
2019-10-18 22:16:28 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+client.login('NTU5NDM5MDg1MDgxNDYwNzc1.XaocQw.1bTFmziBeZWNRioeX7UscL_wMDE');
